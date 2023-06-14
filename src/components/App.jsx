15475346b30c250dcda2aa8b3c1ba45fdd2e1e8a
ReXeR00 <106077454+ReXeR00@users.noreply.github.com>
2023-06-14 12:34:08 +0000

    Add Files Jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,41 @@
+import  Section  from './Section/Section';
+import UserProfile from './UserProfile/UserProfile';
+import  Statistics  from './Statistics/Statistics';
+import  FriendList  from './FriendList/FriendList';
+import  TransactionHistory  from './TransactionHistory/TransactionHistory';
+import user from '../data/user.json';
+import uploadStats from '../data/uploadStats.json';
+import friends from '../data/friends.json';
+import transactions from '../data/transactions.json';
+
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Section title={"Task-1 'Social network profile'"}>
+        <UserProfile
+          username={user.username}
+          tag={user.tag}
+          location={user.location}
+          avatar={user.avatar}
+          stats={user.stats}
+        />
+      </Section>
+
+      <Section title={"Task-2 'Statistics section'"}>
+        <Statistics title="Upload stats" stats={uploadStats} />
+      </Section>
+      <Section>
+        <Statistics stats={uploadStats} />
+      </Section>
+
+      <Section title={"Task-3 'Friend list'"}>
+        <FriendList friends={friends} />
+      </Section>
+
+      <Section title={"Task-4 'Transaction history'"}>
+        <TransactionHistory transactions={transactions} />
+      </Section>
     </div>
   );
 };
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,38 @@
+import PropTypes from 'prop-types';
+import css from './Statistics.module.css';
+
+const randomColor = () => {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+export default function Statistics ({ title, stats })  {
+  return (
+    <div className={css.Statistics}>
+      {title && <h2 className={css.title}>{title}</h2>}
+      <ul className={css.statList}>
+        {stats.map(({ id, label, percentage }) => {
+          return (
+            <li
+              className={css.item}
+              key={id}
+              style={{ backgroundColor: randomColor() }}
+            >
+              <span className={css.label}>{label}</span>
+              <span className={css.percentage}>{percentage} %</span>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      label: PropTypes.string,
+      percentage: PropTypes.number,
+    })
+  ),
+};
